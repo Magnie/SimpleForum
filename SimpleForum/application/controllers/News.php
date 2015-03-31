5e6b2cad4f501c5b6dcad220f19a0337038440d7
Joseph Augustine <MagnieTheReal@gmail.com>
2015-03-30 22:57:15 -0600

    CodeIgniter News Tutorial - News Section - Error Fix 2

--- SimpleForum/application/controllers/News.php
+++ SimpleForum/application/controllers/News.php
@@ -18,14 +18,14 @@ class News extends CI_Controller {
     public function view($slug = NULL) {
         $data['news_item'] = $this->news_model->get_news($slug);
         
-        $if (empty($data['news_item'])) {
+        if (empty($data['news_item'])) {
             show_404();
         }
         
-        $data['title'] = data['news_item']['title'];
+        $data['title'] = $data['news_item']['title'];
         
         $this->load->view('templates/header', $data);
         $this->load->view('news/view', $data);
-        $this-load->view('templates/footer');
+        $this->load->view('templates/footer');
     }
 }
