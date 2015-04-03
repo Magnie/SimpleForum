462626699a0fa63c83e52b4041eca53d4e9e9d2e
Joseph Augustine <MagnieTheReal@gmail.com>
2015-04-03 15:11:05 -0600

    Initial start. Supports Categories.

---
+++ application/models/forum_model.php
@@ -0,0 +1,39 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Forum_model extends CI_Model {
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+    
+    public function get_category($category) {
+        $limit = 1;
+        $query = $this->db->get_where('sf_categories', array('id' => $category), $limit)->result();
+        if (!$query) {
+            $query = array(null);
+        }
+        return $query[0];
+    }
+    
+    public function get_categories($parent = 'home') {
+        $limit = 128;
+        $offset = 0;
+        $query = $this->db->get_where('sf_categories', array('parent' => $parent), $limit, $offset);
+        return $query->result();
+    }
+    
+    public function get_threads($category, $page) {
+        $limit = 20;
+        $offset = $page * $limit;
+        $query = $this->db->get_where('sf_topics', array('category' => $category), $limit, $offset);
+        return $query->result();
+    }
+    
+    public function get_posts($thread, $page) {
+        $limit = 40;
+        $offset = $page * $limit;
+        $query = $this->db->get_where('sf_posts', array('topic' => $thread), $limit, $offset);
+        return $query->result();
+    }
+}
---
+++ application/views/home.php
@@ -0,0 +1,9 @@
+        <div class="section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>Home Page! :D</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
