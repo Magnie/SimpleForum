fa8841caf1040b77f2417d8bb3799935946ab2c9
Joseph Augustine <MagnieTheReal@gmail.com>
2015-04-03 15:26:06 -0600

    Initial start. Supports Categories.

--- application/views/home.php
+++
@@ -1,9 +0,0 @@
-        <div class="section">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h1>Home Page! :D</h1>
-                    </div>
-                </div>
-            </div>
-        </div>
